f4ea5ec4ca2406e2282cd95af33fe833e8e1d8a0
Pepe <ppiatek@sointeractive.pl>
2017-07-03 23:33:16 +0200

    add menu to calc

--- routes/calcRoute.js
+++ routes/calcRoute.js
@@ -1,50 +1,75 @@
 var express = require('express');
 var router = express.Router();
-var parseString = require('xml2js').parseString;
-var http = require('http');
-var request = require('request');
-var url = require('url');
+var fs = require('fs');
 
-var now = new Date();
-var currentYear = now.getFullYear();   
-var currentMonth = now.getMonth() +1;
-var currentDay = now.getDate();
-var currentHour = now.getHours();
-var ascii = require('../lib/ascii');
+const comongo = require('co-mongo');
+const co = require('co');
+const foreach = require('generator-foreach')
+const Promise = require("bluebird");
 
 const q = require('q');
 const moment = require('moment');
 
-router.get('/', (req, res) => q.async(function * () {
-    const title = "AMoney Nowe Oblicze Finansów";
-    const description = "AMoney to zaawansowany kalkulator finansowy, który opiera się na danych Narodowego Banku Polskiego. Sprawdź co oferuje.";
+router.get('/', (req, res) => {
+    co(function *() {
+        let db = yield comongo.connect('mongodb://127.0.0.1:27017/products');
+        let collection = yield db.collection('productsList');   
+        let products =  yield collection.find({alive: true}).toArray();
+        let categories = [...new Set(products.map(product => product.category))];
+        let subcategories = [...new Set(products.map(product => product.subcategory))];
+        let names = [...new Set(products.map(product => product.name))];
+        let productList;
 
-    let selectedDate = moment().format('YYYY-MM-DD');
+        let categoriesMap = {}; 
+        let subCategoriesMap = {};
+        yield Promise.each(categories, co.wrap(function*(category) {
+            categoriesMap[category] = yield collection.find({category: category}).toArray();
+        }));
 
-    const day = selectedDate.split('-')[2];
-    const month = selectedDate.split('-')[1];
-    const year = selectedDate.split('-')[0];
+        yield Promise.each(subcategories, co.wrap(function*(subcategory) {
+            subCategoriesMap[subcategory] = yield collection.find({subcategory: subcategory}).toArray();
+        }));
 
-    let nbpTable = yield getContent(`http://api.nbp.pl/api/exchangerates/tables/a/${selectedDate}`);
-    
-    while (nbpTable === '404 NotFound - Not Found - Brak danych') {
-        selectedDate = moment(selectedDate).add(-1,'days').format('YYYY-MM-DD');
-        nbpTable = yield getContent(`http://api.nbp.pl/api/exchangerates/tables/a/${selectedDate}`);
-    }
+        categories.forEach( category => {
+            categoriesMap[category] = [...new Set (categoriesMap[category].filter(product => product.alive).map(product => product.subcategory))];
+        })
 
-    res.render('calcStartPage', {
-        nbpTable: JSON.parse(nbpTable)[0].rates,
-        title: title, 
-        description: description,
-        amount: req.query.amount,
-        year: year - 2000,
-        month: month,
-        day: day,
-        date: selectedDate,
-        selected1: 'PLN',
-        selected2: 'EUR'
-    });
-})());
+        subcategories.forEach( subcategory => {
+            subCategoriesMap[subcategory] = [...new Set (subCategoriesMap[subcategory].map(product => product.name))];
+        })
+
+        const title = "AMoney Nowe Oblicze Finansów";
+        const description = "AMoney to zaawansowany kalkulator finansowy, który opiera się na danych Narodowego Banku Polskiego. Sprawdź co oferuje.";
+
+        let selectedDate = moment().format('YYYY-MM-DD');
+
+        const day = selectedDate.split('-')[2];
+        const month = selectedDate.split('-')[1];
+        const year = selectedDate.split('-')[0];
+
+        let nbpTable = yield getContent(`http://api.nbp.pl/api/exchangerates/tables/a/${selectedDate}`);
+        
+        while (nbpTable === '404 NotFound - Not Found - Brak danych') {
+            selectedDate = moment(selectedDate).add(-1,'days').format('YYYY-MM-DD');
+            nbpTable = yield getContent(`http://api.nbp.pl/api/exchangerates/tables/a/${selectedDate}`);
+        }
+
+        res.render('calcStartPage', {
+            nbpTable: JSON.parse(nbpTable)[0].rates,
+            title: title, 
+            description: description,
+            amount: req.query.amount,
+            year: year - 2000,
+            month: month,
+            day: day,
+            date: selectedDate,
+            selected1: 'PLN',
+            selected2: 'EUR',
+            categoriesMap: categoriesMap,
+            categoriesDictionary: categories,
+        });
+    }).catch(err => console.log(err))
+});
 
 router.post('/:from-na-:on--:from-ile-to-:on', (req, res) => q.async(function * () {
     const title = "Przelicznik" + " " + req.params.from + " na " + req.params.on + ". " + req.params.from + " ile to " + req.params.on;
@@ -82,38 +107,69 @@ router.post('/:from-na-:on--:from-ile-to-:on', (req, res) => q.async(function * 
     res.json({ course: course.toFixed(2), date: selectedDate, title: title, description: description });
 })());
 
-router.get('/:from-na-:on--:from-ile-to-:on', (req, res) => q.async(function * () {
-    const title = "Przelicznik" + " " + req.params.from + " na " + req.params.on + ". " + req.params.from + " ile to " + req.params.on;
-    const description = title + " ? Sprawnie obliczysz to za pomocą kalkulatora Amoney. Obliczenia oparte o kursy NBP. Sprawdź";
+router.get('/:from-na-:on--:from-ile-to-:on', (req, res) => {
+    co(function *() {
+        let db = yield comongo.connect('mongodb://127.0.0.1:27017/products');
+        let collection = yield db.collection('productsList');   
+        let products =  yield collection.find({alive: true}).toArray();
+        let categories = [...new Set(products.map(product => product.category))];
+        let subcategories = [...new Set(products.map(product => product.subcategory))];
+        let names = [...new Set(products.map(product => product.name))];
+        let productList;
 
-    let selectedDate = moment().format('YYYY-MM-DD');
+        let categoriesMap = {}; 
+        let subCategoriesMap = {};
+        yield Promise.each(categories, co.wrap(function*(category) {
+            categoriesMap[category] = yield collection.find({category: category}).toArray();
+        }));
 
-    const day = req.query.day;
-    const month = req.query.month;
-    const year = '20' + req.query.year;
+        yield Promise.each(subcategories, co.wrap(function*(subcategory) {
+            subCategoriesMap[subcategory] = yield collection.find({subcategory: subcategory}).toArray();
+        }));
 
-    let nbpTable = yield getContent(`http://api.nbp.pl/api/exchangerates/tables/a/${selectedDate}`);
-    
-    while (nbpTable === '404 NotFound - Not Found - Brak danych') {
-        selectedDate = moment(selectedDate).add(-1,'days').format('YYYY-MM-DD');
-        nbpTable = yield getContent(`http://api.nbp.pl/api/exchangerates/tables/a/${selectedDate}`);
-    }
+        categories.forEach( category => {
+            categoriesMap[category] = [...new Set (categoriesMap[category].filter(product => product.alive).map(product => product.subcategory))];
+        })
 
-    const canonical = Object.keys(req.query).length ? `http://amoney.pl/przelicznik/${req.params.from}-na-${req.params.on}--${req.params.from}-ile-to-${req.params.on}` : '';
-    res.render('calcMain', {
-        nbpTable: JSON.parse(nbpTable)[0].rates,
-        title: title, 
-        description: description,
-        year: year - 2000,
-        month: month,
-        day: day,
-        date: selectedDate,
-        selected1: req.params.from,
-        selected2: req.params.on,
-        amount: req.query.amount,
-        canonical: canonical,
-    });
-})());
+        subcategories.forEach( subcategory => {
+            subCategoriesMap[subcategory] = [...new Set (subCategoriesMap[subcategory].map(product => product.name))];
+        })
+
+        const title = "Przelicznik" + " " + req.params.from + " na " + req.params.on + ". " + req.params.from + " ile to " + req.params.on;
+        const description = title + " ? Sprawnie obliczysz to za pomocą kalkulatora Amoney. Obliczenia oparte o kursy NBP. Sprawdź";
+
+        let selectedDate = moment().format('YYYY-MM-DD');
+
+        const day = req.query.day;
+        const month = req.query.month;
+        const year = '20' + req.query.year;
+
+        let nbpTable = yield getContent(`http://api.nbp.pl/api/exchangerates/tables/a/${selectedDate}`);
+        
+        while (nbpTable === '404 NotFound - Not Found - Brak danych') {
+            selectedDate = moment(selectedDate).add(-1,'days').format('YYYY-MM-DD');
+            nbpTable = yield getContent(`http://api.nbp.pl/api/exchangerates/tables/a/${selectedDate}`);
+        }
+
+        const canonical = Object.keys(req.query).length ? `http://amoney.pl/przelicznik/${req.params.from}-na-${req.params.on}--${req.params.from}-ile-to-${req.params.on}` : '';
+
+        res.render('calcMain', {
+            nbpTable: JSON.parse(nbpTable)[0].rates,
+            title: title, 
+            description: description,
+            year: year - 2000,
+            month: month,
+            day: day,
+            date: selectedDate,
+            selected1: req.params.from,
+            selected2: req.params.on,
+            amount: req.query.amount,
+            canonical: canonical,
+            categoriesMap: categoriesMap,
+            categoriesDictionary: categories,
+        });
+    })
+});
 
 module.exports = router;
 
