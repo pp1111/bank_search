685fed21d28aacdfb5349e7b47b940f32f0bc3a1
root <root@vps271331.ovh.net>
2017-07-03 13:09:17 +0200

    add images

--- public/images/products/rename.js
+++ public/images/products/rename.js
@@ -3,6 +3,6 @@ var fs = require('fs');
 var dir = fs.readdirSync(__dirname);
 
 dir.forEach(file => {
-	fs.renameSync(__dirname + `/${file}`, __dirname + `/${file}`.replace(/ /g, "-"))
+	fs.renameSync(__dirname + `/${file}`, __dirname + `/${file}`.replace(/ /g, "-").replace(/ż/g, 'ż').replace(/ó/g, 'ó'));
 	console.log(file);
 })
